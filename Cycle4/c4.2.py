bfa39c0c5950028445670b7c98ae66c5492bbdc2
AnjuVarghese12345 <93638518+AnjuVarghese12345@users.noreply.github.com>
2022-03-02 17:35:21 +0530

    Update c4.2.py

--- Cycle4/c4.2.py
+++ Cycle4/c4.2.py
@@ -1,23 +1,23 @@
-class account:
-	def __init__(self):
-		self.balance=0
-
-	def deposit(self):
-		amount=float(input("Enter amount to be Deposited: "))
-		self.balance += amount
-		print("\n Amount Deposited:",amount)
-
-	def withdraw(self):
-		amount = float(input("Enter amount to be Withdrawn: "))
-		if self.balance>=amount:
-			self.balance-=amount
-			print("\n You Withdrew:", amount)
-		else:
-			print("\n Insufficient balance ")
-
-	def display(self):
-		print("\n Net Available Balance=",self.balance)
-s =account()
-s.deposit()
-s.withdraw()
-s.display()
+class Bankaccount:
+    def _init_(self):
+        self.accno=10253515421
+        self.name="Anju"
+        self.acctype="savings"
+        self.balance=0
+    def deposit(self):
+        amount=int(input("enter the amount to be deposited: "))
+        self.balance+=amount
+        print("Amount succsfully deposited")
+    def withdraw(self):
+        amount=int(input("enter the amount to withdraw: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("succesfully withdrawn")
+        else:
+            print("insufficient balance")
+    def display(self):
+        print("Account number=",self.accno,"\nName=",self.name,"\nAccount type=",self.acctype,"\nAvailable balance=",self.balance)
+ob=Bankaccount()
+ob.deposit()
+ob.withdraw()
+ob.display()
