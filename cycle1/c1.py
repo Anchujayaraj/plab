8e1e53e500bd359c342f9c0adb3be2c7f3c6ea22
Anchujayaraj <94031142+Anchujayaraj@users.noreply.github.com>
2022-03-03 22:22:46 +0530

    Delete cycle1 directory

--- cycle1/c1.py
+++
@@ -1,8 +0,0 @@
-r=int(input("Enter the radius oif the circle:"))
-import math
-math.pi
-pi=3.14
-peri=2*pi*r
-print("Perimeter of the circle is:",peri)
-area=pi*r*r
-print("Area is:",area)
--- cycle1/c3.py
+++
@@ -1,5 +0,0 @@
-a=4
-b=5
-print("Before swapping",a,b)
-a,b=b,a
-print( "After swapping","a:",a,"b:",b)
